3601372ce9f9ce62a4505e1f699e8fa69783ec2c
Derek Baker <derek.baker89@gmail.com>
2019-08-28 12:05:36 +0100

    create handler function

--- server.js
+++ server.js
@@ -1,6 +1,17 @@
 const http = require('http')
 
-const server = http.createServer();
+const message = "We are a great team! boom!"
+
+const handler = (req, res) => {
+	res.writeHead(200, {"Content-Type": "text/html"})
+	console.log("i'm res", res)
+	res.write(message)
+	console.log("i'm readable", res.statusCode)
+	res.end()
+}
+
+
+const server = http.createServer(handler);
 const port = process.env.PORT || 3000
 server.listen(port,  () => {
 	console.log(`The server is listening on ${port}`)})
